7b89808d61eadfb12920dc3f414c7c6c9c26b52c
Lynda <lb23330019@myu.universitecentrale.tn>
2026-07-21 02:35:27 +0200

    Fix PORT to 8181 for Azure

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 app.use(require('./middleware/errorMiddleware').notFound);
 app.use(require('./middleware/errorMiddleware').errorHandler);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
